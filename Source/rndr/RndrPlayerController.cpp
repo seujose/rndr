eb2b2d8ddc2c2be7272376cff46feb3555e3e655
Jose Jacques <jacques.jose@gmail.com>
2019-05-31 13:15:24 -0300

    master widget

---
+++ Source/rndr/RndrPlayerController.cpp
@@ -0,0 +1,2 @@
+#include "RndrPlayerController.h"
+
---
+++ Source/rndr/RndrPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "RndrPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RNDR_API ARndrPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
