46cdfab9bdcafcbd651c6b7c932e5ec5dc9d2009
Jose Jacques <jacques.jose@gmail.com>
2019-07-09 19:41:24 -0300

    engine plugins cleanup

--- Source/rndr/rndr.Build.cs
+++ Source/rndr/rndr.Build.cs
@@ -5,25 +5,16 @@
 //https://answers.unrealengine.com/questions/70019/how-to-include-the-header-file-from-a-plugin.html
 
 using UnrealBuildTool;
+using System.IO;
 
 public class rndr : ModuleRules
 {
 	public rndr(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EditableMesh" });
-        
         PrivateDependencyModuleNames.AddRange(new string[] {});
         PublicIncludePaths.AddRange(new string[] { "E:\\UnrealEngine\\Engine\\Plugins\\Runtime\\EditableMesh\\Source\\EditableMesh\\Public",  "C:\\rndr\\Source\\assimp", "C:\\Program Files\\Chaos Group\\V-Ray\\AppSDK\\cpp\\include" });
         PublicAdditionalLibraries.AddRange(new string[] { "C:\\rndr\\Binaries\\Win64\\assimp-vc140-mt.lib", "C:\\Program Files\\Chaos Group\\V-Ray\\AppSDK\\cpp\\lib\\VRaySDKLibrary.lib" });
-
-        // Uncomment if you are using Slate UI
-        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-
-        // Uncomment if you are using online features
-        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
     }
 }
